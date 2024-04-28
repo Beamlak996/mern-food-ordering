f73e4a464263e85d3538522dee14f6e852a22e00
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-04-28 03:03:33 -0900

    Create user API

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express"
 import cors from 'cors'
 import mongoose from "mongoose"
 
+import userRouter from "./routes/user.route"
+
 mongoose.connect(process.env.MONGO_URL as string).then(()=> {
     console.log("Connected to database.")
 })
@@ -12,9 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/test", async (req: Request, res: Response)=> {
-    res.json({ message: "Hello!" })
-})
+app.use("/api/my/user", userRouter)
 
 app.listen(7000, ()=> {
     console.log("Server is listening to port 7000")
